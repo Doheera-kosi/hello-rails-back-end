fc77376d5695ef1f20daf4841503e6739f56a577
Doheera-kosi <kupourevans99@gmail.com>
2023-03-05 11:47:32 +0000

    Setup rails backend for react frontend

--- app/controllers/messages_controller.rb
+++
@@ -1,6 +0,0 @@
-class MessagesController < ApplicationController
-  def random_greeting
-    message = Message.order("RANDOM()").first
-    render json: { greeting: message.greeting }
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get 'messages/random_greeting'
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
+  end
 end
